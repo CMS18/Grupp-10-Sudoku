b43dd7cd8315fc5a48fca2c1842a13e5ab8c368d
lovehaglund <lovehaglund@live.se>
2018-09-20 12:59:58 +0200

    Lagat fel

---
+++ GruppUppgift 3/World.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GruppUppgift_3
+{
+    class World
+    {
+        // Skapa en karaktär ( Player )
+
+        // Game-loop
+        //       v
+        // skriver ut vad player ser
+        //       v
+        // skriv ett kommando ( rutiner ) 
+        //       v
+        // om man blir klar // finished ------------> win
+        //       v
+        // om inte klar, gå tillbaka till gameloop. 
+    }
+}
---
+++ GruppUppgift 3/items.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GruppUppgift_3
+{
+    public class items
+    {
+    }
+}
