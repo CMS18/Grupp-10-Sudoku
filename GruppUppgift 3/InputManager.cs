79a3c1bd029a4ef05bdcfcadcf3c2a683296667d
rickard uttermalm <rickard.uttermalm@gmail.com>
2018-09-26 15:50:05 +0200

    Fortsättning på inputmanagerklassen

--- GruppUppgift 3/InputManager.cs
+++ GruppUppgift 3/InputManager.cs
@@ -8,21 +8,42 @@ namespace GruppUppgift_3
 {
     public class InputManager
     {
-        private static List<string> KeyWords = new List<string> { "get", "use", "north", "west", "south", "east", "inventory" };
+        private static List<string> KeyWords = new List<string> { "take", "use", "go", "inventory" };
 
         public static void GetUserInput(string input)
         {
             input = input.ToLower();
+            var inputarray = input.Split(' ');
             
             if (!KeyWords.Contains(input))
             {
                 Console.WriteLine("Invalid command");
             }
-
-
-
+            if (inputarray[0]=="go")
+            {
+                CheckForGoCommands(inputarray[1]);
+            }
+            
         }
 
+        private static void CheckForGoCommands(string input)
+        {
+            if (input == "east")
+            {
 
+            }
+            else if (input == "west")
+            {
+
+            }
+            else if (input == "north")
+            {
+
+            }
+            else if (input == "south")
+            {
+
+            }
+        }
     }
 }
