1cd2b2e4109b7e367729e43ee48385bb466c1e0d
Hannibal Ftaw <Hannibal_rebok@hotmail.com>
2018-09-27 14:02:40 +0200

    Kod som inte är klar

--- GruppUppgift 3/Worldbuilder.cs
+++ GruppUppgift 3/Worldbuilder.cs
@@ -9,11 +9,12 @@ namespace GruppUppgift_3
     public class Worldbuilder
     {
 
-        public Player player1 { get; set; } = new Player();       
+        public Player player1 { get; set; } = new Player();
         public string Name { get; set; }
-        public Room livingRoom = new Room("Living room", "Dark and fuckery");
-        public Room kitchen = new Room("Kitchen",
-                "The flooring is white marble, to the north of you there is a" + 
+        public Room livingRoom = new Room("Living room:", "Riddled with cobwebs and everything is covered" +
+            " by a thin layer of dust. It appears that no one has been in this room for quite some time.");
+        public Room kitchen = new Room("Kitchen:",
+                "The flooring is white marble, to the north of you there is a" +
                 " vintage-looking mahogany door. On the floor infront of you there is a old key.");
 
         public Worldbuilder(string name)
@@ -21,15 +22,15 @@ namespace GruppUppgift_3
 
             Name = name;
 
-            
+
             player1.ChangePosition(kitchen);
 
             Door Door1 = new Door(false, 1, livingRoom, "north");                                                           //exits läggs till i rummen
             kitchen.AddExit(Door1);
             Door Door2 = new Door(false, 2, kitchen, "south");
             livingRoom.AddExit(Door2);
-            
-            
+
+
 
             Key RustyKey = new Key("Rusty key", "Opens nothing", "Useless", false); //items läggs i rummen
             kitchen.roomItems.Add(RustyKey);
@@ -48,9 +49,12 @@ namespace GruppUppgift_3
                 Console.WriteLine();
                 Console.Write("Give your character a name: ");
                 player1.Name = Console.ReadLine();
+                Console.WriteLine($"Hi {player1.Name}! Welcome to the Text Adventure V1.02. To start the game," +
+                    $" type in \r\nSTART. To access the command menu, type in \r\nHELP. Enjoy!");
 
                 while (player1.Alive)
                 {
+
                     Console.WriteLine(player1.CurrentPosition.Name);
                     Console.WriteLine(player1.CurrentPosition.Description);
                     Console.WriteLine($"Choose your next move : ");
@@ -58,7 +62,49 @@ namespace GruppUppgift_3
                     var command = InputManager.GetUserInput(Console.ReadLine());
                     DoStuff(command);
                 }
-            }
+
+
+                    //}
+
+                    //while (player1.Alive)
+                    //{
+                    //    if (player1.Alive && "start" == Console.ReadLine().ToLower())
+                    //    {
+                    //        Console.WriteLine(player1.CurrentPosition.Name);
+                    //        Console.WriteLine(player1.CurrentPosition.Description);
+                    //        Console.WriteLine($"Choose your next move : ");
+                    //        var command = InputManager.GetUserInput(Console.ReadLine());
+                    //        DoStuff(command);
+                    //        Console.WriteLine();
+
+                    //    }
+                    //    else if (player1.Alive && "help" == Console.ReadLine().ToLower())
+                    //    {
+                    //        Console.WriteLine($"Commands: \r\ngo \r\nsouth \r\nnorth \r\nwest \r\neast" +
+                    //        $" \r\nhelp \r\ntake \r\ndrop\r\ninventory");
+                    //        Console.WriteLine($"Choose your next move: ");
+                    //        var command = InputManager.GetUserInput(Console.ReadLine());
+                    //        DoStuff(command);
+
+
+                    //    }
+                    //    else if (player1.Alive)
+                    //    {
+                    //        Console.WriteLine(player1.CurrentPosition.Name);
+                    //        Console.WriteLine(player1.CurrentPosition.Description);
+                    //        Console.WriteLine($"Choose your next move : ");
+                    //        var command = InputManager.GetUserInput(Console.ReadLine());
+                    //        DoStuff(command);
+                    //    }
+                    //    else
+                    //    {
+                    //        Console.WriteLine("fel");
+                    //    }
+
+
+                    //}
+
+                }
         }
 
         private void DoStuff(string input)
@@ -67,7 +113,7 @@ namespace GruppUppgift_3
             var inputArray = input.Split(' ');
             if (inputArray[0] == "go")
             {
-                if(CheckForDoor(inputArray[1], out Room room))
+                if (CheckForDoor(inputArray[1], out Room room))
                 {
                     player1.ChangePosition(room);
                 }
@@ -86,7 +132,7 @@ namespace GruppUppgift_3
                 {
                     room = item.Leadsto;
                     return true;
-                    
+
                 }
             }
             room = null;
